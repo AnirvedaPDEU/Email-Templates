4d46c61e89cb3cc92c6c4ef2a16a6251bc7f3c3c
Dev Parikh <51128342+Dev79844@users.noreply.github.com>
2023-01-30 16:55:15 +0530

    added: email templates

---
+++ script.js
@@ -0,0 +1,10 @@
+const button = document.querySelector('button')
+
+button.addEventListener('click', () => {
+    Notification.requestPermission().then(perm => {
+        if(perm === 'granted'){
+            new Notification("Notif")
+            console.log("notif")
+        }
+    })
+})
